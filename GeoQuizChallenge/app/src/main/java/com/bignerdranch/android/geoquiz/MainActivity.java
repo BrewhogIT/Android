ec950ad63baf2d6fabc3261f212a24ab614b2ce8
BrewhogIT <ssslll69@gmail.com>
2019-04-08 15:45:55 +0500

    quiz app

--- GeoQuizChallenge/app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.java
+++ GeoQuizChallenge/app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.java
@@ -2,6 +2,7 @@ package com.bignerdranch.android.geoquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
@@ -17,6 +18,10 @@ public class MainActivity extends AppCompatActivity {
     private TextView mQuestionTextView;
     private Toast myToast;
     private int mCurrentIndex = 0;
+    private static String TAG = "MainActivity";
+    private static String KEY_INDEX = "index";
+    private int mCorrectAnswer = 0;
+    private static String KEY_ANSWER ="answer";
 
     private Question[] mQuestionBank = new Question[]{
             new Question(R.string.question_australia,true),
@@ -31,13 +36,26 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG,"onCreate() called");
         setContentView(R.layout.activity_main);
+
+        if (savedInstanceState != null){
+            mCurrentIndex = savedInstanceState.getInt(KEY_INDEX,0);
+            mCorrectAnswer = savedInstanceState.getInt(KEY_ANSWER,0);
+        }
 
         mTrueButton = (Button) findViewById(R.id.true_button);
         mTrueButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 checkAnswer(true);
+                mTrueButton.setEnabled(false);
+                mFalseButton.setEnabled(false);
+
+                if (mCurrentIndex == mQuestionBank.length-1){
+                    showResult();
+                    mCorrectAnswer = 0;
+                }
             }
         });
 
@@ -46,6 +64,13 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 checkAnswer(false);
+                mTrueButton.setEnabled(false);
+                mFalseButton.setEnabled(false);
+
+                if (mCurrentIndex == mQuestionBank.length-1){
+                    showResult();
+                    mCorrectAnswer = 0;
+                }
             }
         });
 
@@ -65,6 +90,8 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
                 updateQuestion();
+                mTrueButton.setEnabled(true);
+                mFalseButton.setEnabled(true);
             }
         });
 
@@ -79,6 +106,44 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    @Override
+    public void onStart(){
+        super.onStart();
+        Log.d(TAG,"onStart() called");
+    }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+        Log.d(TAG,"onResume() called");
+    }
+
+    @Override
+    public void onPause(){
+        super.onPause();
+        Log.d(TAG,"onPause() called");
+    }
+
+    @Override
+    public void onStop(){
+        super.onStop();
+        Log.d(TAG,"onStop() called");
+    }
+
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+        Log.d(TAG,"onDestroy() called");
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle bundle){
+        super.onSaveInstanceState(bundle);
+        Log.i(TAG,"onSaveInstanceState() called");
+        bundle.putInt(KEY_INDEX,mCurrentIndex);
+        bundle.putInt(KEY_ANSWER,mCorrectAnswer);
+    }
+
     private void updateQuestion(){
         final int question = mQuestionBank[mCurrentIndex].getTextResId();
         mQuestionTextView.setText(question);
@@ -88,9 +153,21 @@ public class MainActivity extends AppCompatActivity {
         boolean unswerIsTrue = mQuestionBank[mCurrentIndex].isAnswerTrue();
         int messageResId = R.string.incorrect_toast;
 
-        if (unswerIsTrue == userPressedTrue) messageResId = R.string.correct_toast;
+        if (unswerIsTrue == userPressedTrue){
+            messageResId = R.string.correct_toast;
+            mCorrectAnswer ++;
+        }
 
         myToast = Toast.makeText(this,messageResId,Toast.LENGTH_SHORT);
+        myToast.setGravity(Gravity.TOP,0,160);
+        myToast.show();
+    }
+
+    private void showResult(){
+        int percent = mCorrectAnswer * 100 / mQuestionBank.length;
+        String answer = percent + "%";
+        
+        myToast = Toast.makeText(this,answer,Toast.LENGTH_SHORT);
         myToast.setGravity(Gravity.TOP,0,160);
         myToast.show();
     }
