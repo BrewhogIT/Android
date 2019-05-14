feaa2da688402b9382a12877c747700e156efe97
BrewhogIT <ssslll69@gmail.com>
2019-05-14 09:00:47 +0500

    ViewPager in CrimeApp

--- CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.java
+++ CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.java
@@ -50,7 +50,7 @@ public class CrimeListFragment extends Fragment {
         @Override
         public void onClick(View v) {
             position = getAdapterPosition();
-            Intent intent = CrimeActivity.newIntent(getActivity(),mCrime.getId());
+            Intent intent = CrimePagerActivity.newIntent(getActivity(),mCrime.getId());
             startActivity(intent);
         }
     }
