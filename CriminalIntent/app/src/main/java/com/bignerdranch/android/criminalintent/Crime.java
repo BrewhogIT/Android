cf60f7674c02f90ae04cd57d1021e2a729b22052
BrewhogIT <ssslll69@gmail.com>
2019-05-31 20:50:30 +0500

    add sql db in CrimeApp

--- CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/Crime.java
+++ CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/Crime.java
@@ -17,7 +17,11 @@ public class Crime {
     private boolean mRequiresPolice;
 
     public Crime() {
-        mId = UUID.randomUUID();
+        this(UUID.randomUUID());
+    }
+
+    public Crime(UUID uuid){
+        mId = uuid;
         mDate = new Date();
     }
 
--- CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/CrimeLab.java
+++ CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/CrimeLab.java
@@ -1,6 +1,13 @@
 package com.bignerdranch.android.criminalintent;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+import com.bignerdranch.android.criminalintent.database.CrimeBaseHelper;
+import com.bignerdranch.android.criminalintent.database.CrimeDbSchema;
+import com.bignerdranch.android.criminalintent.database.CrimeDbSchema.CrimeTable;
 
 import java.util.ArrayList;
 import java.util.Iterator;
@@ -9,45 +16,74 @@ import java.util.UUID;
 
 public class CrimeLab {
     private static CrimeLab sCrimeLab;
-    private List<Crime> mCrimes;
+    private Context mContext;
+    private SQLiteDatabase mDatabase;
 
-    private CrimeLab(Context context){
-        mCrimes = new ArrayList<>();
+    private CrimeLab(Context context) {
+        mContext = context.getApplicationContext();
+        mDatabase = new CrimeBaseHelper(mContext).getWritableDatabase();
     }
 
-    public static CrimeLab get(Context context){
-        if (sCrimeLab == null){
+    public static CrimeLab get(Context context) {
+        if (sCrimeLab == null) {
             sCrimeLab = new CrimeLab(context);
         }
         return sCrimeLab;
     }
 
-    public List <Crime> getCrimes(){
-        return mCrimes;
+    public List<Crime> getCrimes() {
+        return new ArrayList<>();
     }
 
-    public Crime getCrime(UUID id){
-
-        for (Crime someCrime: mCrimes){
-            if (id.equals(someCrime.getId())){
-                return someCrime;
-            }
-        }
+    public Crime getCrime(UUID id) {
 
         return null;
     }
 
-    public void addCrime (Crime c){
-        mCrimes.add(c);
+    public void addCrime(Crime c) {
+        ContentValues values = getContentValues(c);
+        mDatabase.insert(CrimeTable.NAME, null, values);
     }
-    public void delCrime(UUID id){
-        Iterator iterator = mCrimes.iterator();
 
-        while (iterator.hasNext()){
-            Crime someCrime = (Crime) iterator.next();
-            if (id.equals(someCrime.getId())){
-                iterator.remove();
-            }
-        }
+    public void delCrime(UUID id) {
+//        Iterator iterator = mCrimes.iterator();
+//
+//        while (iterator.hasNext()){
+//            Crime someCrime = (Crime) iterator.next();
+//            if (id.equals(someCrime.getId())){
+//                iterator.remove();
+//            }
+//        }
+    }
+
+    public void updateCrime(Crime crime) {
+        String uuidString = crime.getId().toString();
+        ContentValues values = getContentValues(crime);
+
+        mDatabase.update(CrimeTable.NAME, values,
+                CrimeTable.Cols.UUID + " = ?",
+                new String[]{uuidString});
+    }
+
+    private static ContentValues getContentValues(Crime crime) {
+        ContentValues values = new ContentValues();
+        values.put(CrimeTable.Cols.UUID, crime.getId().toString());
+        values.put(CrimeTable.Cols.TITLE, crime.getTitle());
+        values.put(CrimeTable.Cols.DATE, crime.getDate().getTime());
+        values.put(CrimeTable.Cols.SOLVED, crime.isSolved() ? 1 : 0);
+        return values;
+    }
+
+    private Cursor queryCrimes(String whereClause,String[] whereArgs){
+        Cursor cursor = mDatabase.query(
+                CrimeTable.NAME,
+                null,
+                whereClause,
+                whereArgs,
+                null,
+                null,
+                null
+        );
+        return cursor;
     }
 }
